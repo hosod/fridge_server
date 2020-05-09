819d91cdff2d6a2e7480a5bfb512c2c0de09ba37
hosod <hosoda.0406.1997@gmail.com>
2020-05-10 00:53:12 +0900

    devと本番のそれぞれでdbコンテナへの接続を自動で切り替えできるようになtta

--- app/main.go
+++ app/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	// "log"
+	"flag"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/hosod/fridge_server/app/internal"
+	"github.com/hosod/fridge_server/app/internal/database"
 )
 
 func main() {
+	var dev = flag.Bool("dev", false, "please specify -dev flag")
+
+	flag.Parse()
+
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello Wolrd hoge fuga piyo\n")
+		c.String(http.StatusOK, "Hello Wolrd hoge fuga piyo1234\n")
 	})
-
-	v1 := router.Group("api/v1")
-	{
-		v1.POST("/user", users.Register)
-	}
-
+	// v1 := router.Group("api/v1")
+	// {
+	// 	v1.POST("/user", users.Register)
+	// }
+	database.Init(*dev)
+	defer database.Close()
 	router.Run(":9000")
 }
